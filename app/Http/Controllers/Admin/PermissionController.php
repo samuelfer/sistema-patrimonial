e6f3b6ecb8131a6a44b1179d1437389802dce1ee
Samuel <samuelfesant@gmail.com>
2024-01-13 14:21:31 -0300

    Remove import

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreUpdatePermission;
 use App\Models\Permission;
-use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
