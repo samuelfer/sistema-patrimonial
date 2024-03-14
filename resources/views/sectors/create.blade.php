7d9739c29cb0499833f7be3161a4ac56c73f3791
Samuel <samuelfesant@gmail.com>
2024-03-13 22:55:52 -0300

    Add mask input

--- resources/views/sectors/create.blade.php
+++ resources/views/sectors/create.blade.php
@@ -114,3 +114,13 @@
 </div>
 
 @stop
+
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#phone').inputmask('(99)-999-9999');
+        });
+    </script>
+@stop
