a9b25f21af2b97fad0cd93834e30726edf890362
Samuel <samuelfesant@gmail.com>
2024-03-16 12:41:05 -0300

    Validated is admin before creating and retrieving

--- app/Http/Traits/TenantAttributeTrait.php
+++ app/Http/Traits/TenantAttributeTrait.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Traits;
 
+use App\Models\ManagementUnit;
+
 trait TenantAttributeTrait
 {
     public static function bootTenantAttributeTrait()
     {
-        static::creating(function ($model) {
-            $model->management_unit_id = auth()->user()->management_unit_id;
-        });
-        static::retrieved(function ($model) {
-            $model->where('management_unit_id', auth()->user()->management_unit_id);
-        });
+        if(auth()->user() && !auth()->user()->is_admin) {
+            static::creating(function ($model) {
+                if(!$model instanceof ManagementUnit) {
+                    $model->management_unit_id = auth()->user()->management_unit_id;
+                }
+            });
+            static::retrieved(function ($model) {
+                if($model instanceof ManagementUnit) {
+                    $model->where('id', auth()->user()->management_unit_id);
+                } else {
+                    $model->where('management_unit_id', auth()->user()->management_unit_id);
+                }
+            });
+        }
     }
 }
